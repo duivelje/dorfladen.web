26f14d56d9a0781cd21becdf26a913a53fdc042e
Steffi C <stefanie@familie-clement.de>
2023-12-25 15:51:46 +0100

    Initial commit

---
+++ src/test/kotlin/com/steffi/dorfladen/web/WebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.steffi.dorfladen.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
